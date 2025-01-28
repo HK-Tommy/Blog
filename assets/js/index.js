83ecb64b0a57a1d632c2ce60e2c731df57170111
HK 湯米Tommy <tlamhappy10@gmail.com>
2025-01-28 21:27:43 +0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,4 @@ function SearchButton(){
     if (document.getElementById("search").value != ""){
         window.open("https://www.google.com/search?q=site%3Ahttps%3A%2F%2Fhk-tommy.github.io%2FBlog+" + document.getElementById("search").value,'_self');
     }
-}
+} 
